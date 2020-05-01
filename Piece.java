376b81765b424168cdd9871a2e7e25ed5bcd461c
AAspCodes <asp.anthony1@gmail.com>
2020-05-01 14:10:27 -0700

    add move to Piece interface

--- Piece.java
+++ Piece.java
@@ -4,6 +4,7 @@ import java.awt.Graphics;
 public interface Piece {
 	public void draw(Graphics g);
 	public boolean canMove(Direction direction);
+	public void move(Direction direction);
 	public boolean canRotate();
 	public void rotate();
 
